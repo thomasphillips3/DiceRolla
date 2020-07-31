1e827ddf91f83e692c6edd740de79bf0eba8a8bc
Thomas Phillips <thomasphillips3@gmail.com>
2020-07-31 16:55:07 -0700

    display a different image for each random number rolled

--- app/src/main/java/com/thomasphillips3/dicerolla/MainActivity.kt
+++ app/src/main/java/com/thomasphillips3/dicerolla/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        roll_button.text = "Let's Roll"
+        roll_button.text = getString(R.string.roll)
 
         roll_button.setOnClickListener {
             rollDice()
@@ -19,6 +19,15 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         val randomInt =  Random().nextInt(6) + 1
-        result_text.text = randomInt.toString()
+        val drawableResource = when(randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        dice.setImageResource(drawableResource)
     }
 }
