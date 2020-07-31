9357b5211c28b134239b84e27c54f81981432508
Thomas Phillips <thomasphillips3@gmail.com>
2020-07-30 23:47:39 -0700

    roll dice and display value

--- app/src/main/java/com/thomasphillips3/dicerolla/MainActivity.kt
+++ app/src/main/java/com/thomasphillips3/dicerolla/MainActivity.kt
@@ -2,10 +2,23 @@ package com.thomasphillips3.dicerolla
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        roll_button.text = "Let's Roll"
+
+        roll_button.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val randomInt =  Random().nextInt(6) + 1
+        result_text.text = randomInt.toString()
     }
 }
